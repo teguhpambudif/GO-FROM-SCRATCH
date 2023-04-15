ee258a2fdeace4da4f305498528a9f610c068e97
teguh <teguh@kali>
2023-04-16 00:10:08 +0700

    operator

---
+++ 3-operator.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	x, y := 5, 6
+
+	fmt.Println("x + y = ", x+y)
+	fmt.Println("x - y = ", x-y)
+	fmt.Println("x * y = ", x*y)
+	fmt.Println("x / y = ", x/y)
+	fmt.Println("x % y = ", x%y)
+
+	isBool := true
+	hate := false
+
+	fmt.Println(isBool && hate)
+	fmt.Println(isBool || hate)
+	fmt.Println(!isBool)
+}
