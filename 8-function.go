7b1f0e65a771039e86dbdfd4675ac47d10dc4423
teguh <teguh@kali>
2023-04-16 12:25:23 +0700

    func recursion

--- 8-function.go
+++ 8-function.go
@@ -4,10 +4,22 @@ import "fmt"
 
 func main() {
 	x, y := 5, 6
+	num := 5
 
 	fmt.Println(add(x, y))
+	fmt.Println()
+	fmt.Println(factorial(num))
 }
 
 func add(num1 int, num2 int) int {
 	return num1 + num2
+}
+
+func factorial(num int) int {
+
+	if num == 0 {
+
+		return 1
+	}
+	return num * factorial(num-1)
 }
